fffdc752cc1a0e074824af2b2174cef593921dd1
stnDinus <111202214433@mhs.dinus.ac.id>
2023-12-22 13:46:58 +0700

    feat: basic website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,14 @@
-<slot/>
+<script>
+  import "../app.css";
+</script>
 
-<footer>🄯A11.2022.14433</footer>
+<header class="p-5 text-4xl font-black">SO Web Server</header>
+
+<slot />
+
+<footer class="p-5">
+  <div class="flex flex-col">
+    <span>🄯A11.2022.14433</span>
+    <span>🄯A11.2022.14442</span>
+  </div>
+</footer>
